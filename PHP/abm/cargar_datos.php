669ca054bfb6d38ac8ff6fbf9e405a2240eccb51
Debohmonte <debora.monteverde97@gmail.com>
2024-10-20 12:19:28 -0300

    z

--- PHP/abm/cargar_datos.php
+++ PHP/abm/cargar_datos.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);  // Display errors
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once 'db_connection.php';
 
 // Ensure the response is JSON
