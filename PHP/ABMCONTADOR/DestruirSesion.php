86e0133152bcce81f149c9c72d935787fdf44cec
Debohmonte <debora.monteverde97@gmail.com>
2024-10-25 17:51:16 -0300

    b

--- PHP/ABMCONTADOR/DestruirSesion.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION = []; // Clear session variables
-session_destroy(); // Destroy the session
-header('Location: FormLogin.php'); // Redirect to login page
-exit();
-?>
